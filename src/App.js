f13c776e58ced16499ed0bf206ef3453445f053a
Antonio <puhanicantionio@gmailcom>
2022-06-13 15:42:15 +0200

    pocetni css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useRef, useEffect } from 'react';
+import TodoList from './TodoList';
+import { v4 as uuidv4 } from 'uuid'
+import { Route, Router, Routes, useNavigate } from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+    console.log(LOCAL_STORAGE_KEY)
+  }, [todos])
+
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false }]
+    })
+    todoNameRef.current.value = null
+  }
+
+  function handleClearTodos() {
+    const TodosToClear = todos.filter(todo => !todo.complete)
+    setTodos(TodosToClear)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='App'>
+        
+        <Navbar />
+        <div className='content'>
+          <TodoList todos={todos} toggleTodo={toggleTodo} />
+          <input ref={todoNameRef} type="text" onKeyDown={(e) => e.key === 'Enter' && handleAddTodo()} />
+          <button onClick={handleAddTodo} >Add Todo</button>
+          <button onClick={handleClearTodos}>Clear Todo</button>
+          <div>{todos.filter(todo => !todo.complete).length} left to do</div>
+        </div>
+      </div>
+    </>
   );
 }
 
