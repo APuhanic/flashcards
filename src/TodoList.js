e53f0da71d0263c3d8fce65ffa481f45fadccf83
Antonio <puhanicantionio@gmailcom>
2022-06-17 20:16:04 +0200

    read from fire base

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,8 +3,8 @@ import Todo from './Todo'
 
 export default function TodoList({ todos, toggleTodo }) {
     return (
-       todos.map(todo =>{
-            return <Todo key={todo.id} toggleTodo={toggleTodo} todo={todo}/>
-       })
+        todos.map(todo => {
+            return <Todo key={todo.id} toggleTodo={toggleTodo} todo={todo} />
+        })
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,44 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+
+import { useState, useRef, useEffect } from 'react'
+import { useAuth } from "../contexts/authContext";
+
+import { getFirestore, collection, setDoc, doc, getDoc, getDocs, addDoc, deleteDoc } from "firebase/firestore"
+import app from '../firebase/firebase'
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+
+const db = getFirestore(app)
 
 export default function Home() {
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "users");
+  const { currentUser } = useAuth();
+
+
+  useEffect(() => {
+
+    if (currentUser) {
+      const getUsers = async () => {
+        const data = await getDocs(usersCollectionRef);
+        setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      };
+      getUsers();
+    }
+  }, []);
+
   return (
-    <div className='Home'>Home</div>
+    <>
+      <div className='Home'>Home</div>
+      {users && users.map((user) => {
+        return (
+          <div>
+            {" "}
+            <h1>Name: {user.name}</h1>
+          </div>
+        );
+      })}
+    </>
   )
+
 }
