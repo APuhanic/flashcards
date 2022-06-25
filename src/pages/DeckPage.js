94d290b8aa8945584592652ea4741f05b32066f0
Antonio <puhanicantionio@gmailcom>
2022-06-25 20:07:46 +0200

    dodano sučelje za karte i deckove, dodavanje i sl.

--- src/pages/DeckPage.js
+++ src/pages/DeckPage.js
@@ -1,13 +1,73 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Route, Routes, useNavigate, useParams } from 'react-router-dom'
+import { Card, Form, Button, Container } from 'react-bootstrap';
+import { addCard, getCards } from '../firebase/firebasedb';
+import { useAuth } from '../contexts/authContext';
+import FlashcardList from '../components/FlashcardList';
+import Studying from '../pages/Studying'
 
 export default function DeckPage() {
     const deck = useParams();
-    console.log(deck.deckName)
+    const [answer, setAnswer] = useState("");
+    const [question, setQuestion] = useState("");
+    const [flashcards, setFlashcards] = useState([]);
+    const { currentUser } = useAuth();
+
+    const navigate = useNavigate();
+
+    function startStudying() {
+        navigate(`/Home/${deck.id}/studying`);
+    }
+
+    function handleAddNewCard(e) {
+        e.preventDefault()
+        try {
+            addCard(deck.id, answer, question)
+            setFlashcards(prevFlashcards => {
+                return [...prevFlashcards, { answer: answer, question: question }]
+            })
+            setQuestion("")
+            setAnswer("")
+        }
+        catch (error) {
+            console.log(error)
+        }
+
+    }
+
+    useEffect(() => {
+        if (currentUser) {
+            getCards(deck.id).then((res) => {
+                setFlashcards(res);
+            })
+        }
+    }, [currentUser]);
+
 
     return (
         <>
-            
+            <Routes>
+            </Routes>
+            <div className='StartStudyingButton text-center mb-3' >
+                <Button size="lg" onClick={startStudying}> START STUDYING</Button>
+            </div>
+            <Card className='d-flex justify-content-center'>
+                <Card.Body>
+                    <Form onSubmit={handleAddNewCard} >
+                        <Form.Group id="question">
+                            <Form.Control as="textarea" value={question} onChange={(e) => setQuestion(e.target.value)} placeholder="Question" required />
+                        </Form.Group>
+                        <Form.Group id="answer" className='mt-3'>
+                            <Form.Control as="textarea" value={answer} onChange={(e) => setAnswer(e.target.value)} placeholder="Answer" required />
+                        </Form.Group>
+                        <Button disabled={question.length === 0} variant="primary" className='mt-3 mb-3' type="submit">Add Card</Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+            <Container className='w-75 justify-content-center'>
+                {flashcards && <FlashcardList flashcards={flashcards} />}
+            </Container>
+
         </>
     )
 }
